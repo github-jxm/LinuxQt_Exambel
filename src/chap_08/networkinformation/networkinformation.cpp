356d6f6a776477e230c1c55cc9f3a08c1fc7e903
jiang_xmin <jiang_xmin@massclouds.com>
2016-11-11 00:58:07 +0800

    .....

--- src/chap_08/networkinformation/networkinformation.cpp
+++ src/chap_08/networkinformation/networkinformation.cpp
@@ -1,4 +1,5 @@
 #include "networkinformation.h"
+#include <QDebug>
 
 NetworkInformation::NetworkInformation( QWidget *parent, Qt::WindowFlags  f )
     : QDialog( parent, f )
@@ -12,20 +13,20 @@ NetworkInformation::NetworkInformation( QWidget *parent, Qt::WindowFlags  f )
 
     LabelAddress = new QLabel();
     LabelAddress->setText(tr("address:"));
-    
-    LineEditAddress = new QLineEdit();  
-           
+
+    LineEditAddress = new QLineEdit();
+
     PushButtonDetail = new QPushButton( this);
-    PushButtonDetail->setText( tr( "Detail" ) );      
-    
+    PushButtonDetail->setText( tr( "Detail" ) );
+
     QHBoxLayout *hbLayout1 = new QHBoxLayout ();
     hbLayout1->addWidget(LabelLocalHostName);
     hbLayout1->addWidget(LineEditLocalHostName);
-    
+
     QHBoxLayout *hbLayout2 = new QHBoxLayout ();
     hbLayout2->addWidget(LabelAddress);
     hbLayout2->addWidget(LineEditAddress);
-    
+
     QVBoxLayout *vbLayout = new QVBoxLayout( this );
     vbLayout->addLayout( hbLayout1 );
     vbLayout->addLayout( hbLayout2 );
@@ -44,14 +45,26 @@ NetworkInformation::~NetworkInformation()
 void NetworkInformation::getHostInformation()
 {
 	QString localHostName=QHostInfo::localHostName();
-	LineEditLocalHostName->setText(localHostName);
+    LineEditLocalHostName->setText(localHostName);
 	
-	QHostInfo hostInfo = QHostInfo::fromName(localHostName);
-	QList<QHostAddress> listAddress = hostInfo.addresses();
-	if(!listAddress.isEmpty())
-	{
-		LineEditAddress->setText(listAddress.first().toString());
-	}   	
+    QHostInfo hostInfo = QHostInfo::fromName(localHostName);
+    QList<QHostAddress> listAddress = hostInfo.addresses();  // 无线网卡,地址,识别不到
+    //QList<QHostAddress>listAddress = QNetworkInterface::allAddresses();  //所有地址,会取到DHCP分配失败的保留地址
+    if(!listAddress.isEmpty())
+    {
+//        qDebug () <<  " all : " << listAddress;
+        LineEditAddress->setText(listAddress.first().toString());
+        foreach (QHostAddress address,listAddress ) //遍历IP地址
+        {
+            if(address.protocol() == QAbstractSocket::IPv4Protocol){
+                    qDebug() <<"IPV4"<<address.toString();
+            }else if  (address.protocol() == QAbstractSocket::IPv6Protocol){
+                    qDebug()  << "IPV6"<<address.toString();
+            }else{
+                    qDebug()  << "other: "<<address.toString();
+            }
+        }
+    }
 }
 
 void NetworkInformation::slotDetail()
